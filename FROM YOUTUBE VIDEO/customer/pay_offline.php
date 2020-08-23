5c08ef206a82f9e4556ad7930bca38b93b8f76e3
Thiriveedhi Dheeraj <61489415+DHEERAJC007@users.noreply.github.com>
2020-08-23 21:26:41 +0530

    Create pay_offline.php

---
+++ FROM YOUTUBE VIDEO/customer/pay_offline.php
@@ -0,0 +1,27 @@
+<div class="box">
+	<center>
+		<h1>Pay offline using below Method</h1>
+		<p>If you have any questions, please feel free to <a href="../contactus.php">contact us</a>, our customers service center is working for you 24/7.</p>
+	</center>
+	<hr>
+	<div class="table-responsive">
+		<table class="table table-bordered table-hover table-striped">
+			<thead>
+				<tr>
+					<th>Bank Account Details</th>
+					<th>Paypal Id</th>
+					<th>PayuMoney Link</th>
+					<th>PayTm Number</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>319380293884</td>
+					<td>dheerajarya860@gmail.com</td>
+					<td>xxxxxxxxxx</td>
+					<td>929056743</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
