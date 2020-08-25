3f14ac3cbe5bec1a8034c7451b463211587761bb
adityam20 <aditya.maheshwari2019@vitstudent.ac.in>
2020-08-25 15:39:35 +0530

    linked my account
    linked

--- FROM YOUTUBE VIDEO/cart.php
+++ FROM YOUTUBE VIDEO/cart.php
@@ -30,7 +30,7 @@
                             <a href="customer_registration.php">Register</a>
                         </li>
                         <li>
-                            <a href="checkout.php">My Account</a>
+                            <a href="customer/my_account.php">My Account</a>
                         </li>
                         <li>
                             <a href="cart.php">Go to Cart</a>
