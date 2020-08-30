f30baeb5197d9b7f854e0d5fc6083fcf137d479f
VITGEARSHCS <aashish.bansal2019@vitstudent.ac.in>
2020-08-30 20:27:24 +0530

    BUGS FIXED
    ADDED PRODUCT IMAGES. DO NOT FORGET TO REMOVE MY PASSWORD

--- FROM YOUTUBE VIDEO/functions/functions.php
+++ FROM YOUTUBE VIDEO/functions/functions.php
@@ -33,7 +33,7 @@
           global $db;
           $get_p_cats="select * from product_categories";
           $run_p_cats=mysqli_query($db,$get_p_cats);
-          while($row_p_cats=mysqli_fetch_array($run_p_cats)){
+          while($row_p_cats=mysqli_fetch_array($db,$run_p_cats)){
             $p_cat_id=$row_p_cats['p_cat_id'];
             $p_cat_title=$row_p_cats['p_cat_title'];
             echo "<li><a href='shop.php?p_cat=$p_cat_id'>$p_cat_title</a></li>";
